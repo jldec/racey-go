41bb336e91d27b8353214a2989f4afacac4b6fe4
Jurgen Leschner <jldec@ciaosoft.com>
2021-04-25 17:27:44 +0100

    use CounterAtomic instead of count++

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jldec/racey-go
 
 go 1.16
+
+require github.com/jldec/counter-go v1.0.0
--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	counter "github.com/jldec/counter-go"
 )
 
 func main() {
-	var count uint64 = 0
+	count := new(counter.CounterAtomic)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		count++
-		fmt.Fprintln(w, count)
+		count.Inc()
+		fmt.Fprintln(w, count.Get())
 	})
 
 	fmt.Println("Go listening on port 3000")
